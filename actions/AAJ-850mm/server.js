ece10b4729d40f29ce132bba3be56045c1953ece
RicardoFV <48629398+RicardoFV@users.noreply.github.com>
2022-07-11 16:47:09 -0300

    Automatic commit from Bubble

---
+++ actions/AAJ-850mm/server.js
@@ -0,0 +1,18 @@
+function(properties, context) { 
+  // inicializa a variavel
+  var campo = ""
+  // recebo o texto
+  campo = properties.campo
+  // verifico se esta vazio
+  if (campo != "" || campo != null) {
+      // inicializa a variavel
+      let novo = "" 
+      // retiro o caractere especial
+      novo = campo.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+      // realizo o retorno
+      return {
+          resposta : novo.trim()
+      }
+  }
+
+}
